54c364872f6a74c31cbbe43565c7b19483e78d3f
Mustapha Belmokhtar <mbelmokhtar@sqli.com>
2020-10-11 05:03:00 +0100

    add documentation

--- src/main/java/com/musta/belmo/booqs/controller/BookController.java
+++ src/main/java/com/musta/belmo/booqs/controller/BookController.java
@@ -1,6 +1,11 @@
 package com.musta.belmo.booqs.controller;
 
 import com.musta.belmo.booqs.entite.dto.BookDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +19,10 @@ import java.util.Set;
 @RequestMapping(value = "/book", consumes = MediaType.APPLICATION_JSON_VALUE,
 		produces = MediaType.APPLICATION_JSON_VALUE)
 public class BookController {
+	
 	@GetMapping
+	@BookAnnotation
+	@Operation(summary = "Lists all the books")
 	public ResponseEntity<Set<BookDTO>> all() {
 		return ResponseEntity.ok().body(Collections.emptySet());
 	}
